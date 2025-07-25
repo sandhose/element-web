90aacf789cf5f60e6dc08104f5a16bd4b84dbf20
Florian Duros <florian.duros@ormaz.fr>
2025-07-25 16:14:15 +0200

    refactor: move `number.ts` in shared components

--- test/unit-tests/utils/numbers-test.ts
+++ test/unit-tests/utils/numbers-test.ts
@@ -6,7 +6,13 @@ SPDX-License-Identifier: AGPL-3.0-only OR GPL-3.0-only OR LicenseRef-Element-Com
 Please see LICENSE files in the repository root for full details.
 */
 
-import { clamp, defaultNumber, percentageOf, percentageWithin, sum } from "../../../src/utils/numbers";
+import {
+    clamp,
+    defaultNumber,
+    percentageOf,
+    percentageWithin,
+    sum,
+} from "../../../src/shared-components/utils/numbers";
 
 describe("numbers", () => {
     describe("defaultNumber", () => {
